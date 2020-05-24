c1b1c0f834bd81d504f40e2c83ff81b816a6a724
Howard.Lee <yhunglee@gmail.com>
2020-05-24 11:40:33 +0800

    feat(Home.vue): Hide view of HelloWorld component.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,21 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App" /> -->
+    <ChatRoom />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+// import HelloWorld from "@/components/HelloWorld.vue";
+import ChatRoom from "@/components/ChatRoom";
 
 export default {
   name: "Home",
   components: {
-    HelloWorld
-  }
+    // HelloWorld,
+    ChatRoom,
+  },
 };
 </script>
