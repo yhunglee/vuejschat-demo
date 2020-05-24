ba3eae0614662a766e55f8b9aea6458b5a79f73b
Howard.Lee <yhunglee@gmail.com>
2020-05-24 14:32:37 +0800

    feat(Navigator.vue): Add this component for tab-view style.

---
+++ src/components/Navigator.vue
@@ -0,0 +1,9 @@
+<template>
+  <b-card no-body>
+    <b-tabs card>
+      <b-tab title="首頁"></b-tab>
+      <b-tab title="今日話題"></b-tab>
+      <b-tab title="偷偷說"></b-tab>
+    </b-tabs>
+  </b-card>
+</template>
