0a302cb915bd0d487e0f092bd81279a4c344f025
Howard.Lee <yhunglee@gmail.com>
2020-05-24 14:31:58 +0800

    feat(Home.vue): Add Navigator component here.
    Add Navigator component here and code masking for Chat room and Vuejs
    icon.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,21 +1,24 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
+    <!-- <img alt="Vue logo" src="../assets/logo.png" /> -->
     <!-- <HelloWorld msg="Welcome to Your Vue.js App" /> -->
-    <ChatRoom />
+    <Navigator />
+    <!-- <ChatRoom /> -->
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 // import HelloWorld from "@/components/HelloWorld.vue";
-import ChatRoom from "@/components/ChatRoom";
+import Navigator from "@/components/Navigator";
+// import ChatRoom from "@/components/ChatRoom";
 
 export default {
   name: "Home",
   components: {
     // HelloWorld,
-    ChatRoom,
+    Navigator,
+    // ChatRoom,
   },
 };
 </script>
