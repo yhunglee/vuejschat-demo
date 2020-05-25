93d835923eb7bc1c57707f0d491e87b47cb57f7e
Howard.Lee <yhunglee@gmail.com>
2020-05-25 23:33:10 +0800

    feat(Navigator.vue): import ChatRoom component and styling account

--- src/components/Navigator.vue
+++ src/components/Navigator.vue
@@ -3,7 +3,38 @@
     <b-tabs card>
       <b-tab title="首頁"></b-tab>
       <b-tab title="今日話題"></b-tab>
-      <b-tab title="偷偷說"></b-tab>
+      <b-tab title="偷偷說">
+        <ChatRoom />
+      </b-tab>
+      <b-tab title="帳號名稱" disabled />
     </b-tabs>
   </b-card>
 </template>
+
+
+
+<style lang="scss">
+.nav-item:nth-child(4) {
+  // 帳號 position
+  margin-left: 75%;
+  text-align: right;
+  background-color: #21a;
+  .nav-link.disabled {
+    color: #fff;
+  }
+}
+</style>
+
+
+<script lang="ts">
+import Vue from "vue";
+import { Component, Prop } from "vue-property-decorator";
+import ChatRoom from "@/components/ChatRoom.vue";
+
+export default Vue.extend({
+  name: "Navigator" as string,
+  components: {
+    ChatRoom
+  }
+});
+</script>
