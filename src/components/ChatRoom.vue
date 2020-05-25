c08e15561529dc367b66142a64b7e297b9c4e582
Howard.Lee <yhunglee@gmail.com>
2020-05-25 23:30:13 +0800

    feat(ChatRoom.vue): Done input-area layout and img button.

--- src/components/ChatRoom.vue
+++ src/components/ChatRoom.vue
@@ -1,3 +1,85 @@
 <template>
-  <div class="container"></div>
+  <div class="container">
+    <div class="notice-area"></div>
+    <div class="msg-list"></div>
+    <div class="input-area">
+      <div class="action pic">
+        <button>
+          <label>
+            <input type="file" name="imgFile" id="imgFile" accept="image/*" hidden />
+            <b-icon-image></b-icon-image>
+          </label>
+        </button>
+      </div>
+
+      <div class="action emoji-list">
+        <button>
+          <label>
+            <b-icon-emoji-smile></b-icon-emoji-smile>
+          </label>
+        </button>
+      </div>
+
+      <div class="txt msg-input">
+        <textarea name="msg" id="inputMsg" cols="30" rows="1" autofocus></textarea>
+      </div>
+
+      <div class="action send-msg">
+        <button>
+          <b-icon-arrow-right></b-icon-arrow-right>
+        </button>
+      </div>
+    </div>
+  </div>
 </template>
+
+<style lang="scss">
+.container {
+  min-width: 100%;
+  min-height: 100%;
+
+  .input-area {
+    display: flex;
+    width: 100%;
+    align-items: center;
+
+    // 排在這個分頁的最底部
+    position: relative;
+    bottom: 2%;
+    height: 2%;
+
+    button,
+    button:focus {
+      // 取消邊線和聚焦效果
+      border: none;
+      outline: none;
+      font-size: 30px;
+    }
+    .action {
+      // 每個行動佔至少 10%位置，顯得比較開闊
+      min-width: 10%;
+    }
+    .txt.msg-input {
+      width: 65%;
+      padding: 0px 5px;
+      textarea {
+        resize: none;
+        border-radius: 20px;
+        width: 100%;
+        padding: 5px 10px;
+        &:focus {
+          // 取消聚焦效果
+          outline: none;
+        }
+      }
+    }
+  }
+}
+</style>
+
+<script lang="ts">
+import Vue from "vue";
+export default Vue.extend({
+  name: "chat-room" as string
+});
+</script>
