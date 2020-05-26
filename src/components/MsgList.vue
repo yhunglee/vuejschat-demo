0044d7a97b1498f52939eda05ba516781442949c
Howard.Lee <yhunglee@gmail.com>
2020-05-27 01:39:10 +0800

    feat(MsgList.vue): create this for listing messages

---
+++ src/components/MsgList.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="msg-list">
+    <Msg
+      v-bind:msg="{
+        author: 'hello',
+        authorId: '',
+        color: '#f00',
+        createdAt: new Date(),
+        content:
+          'worldqwerqwersdfjsadfjweorqjpwoerjqwerqweoreqerasdfasdfasldfkasld;fkas;dlfkjas;ldfkjas;ldfkjasdl;feworuqpwoeirupedfqwreqwerqwerqwerqwerqwerqwerqwer',
+        isMe: true,
+      }"
+    />
+    <Msg
+      v-bind:msg="{
+        author: 'hello1',
+        authorId: '',
+        color: '#bf0',
+        createdAt: new Date(),
+        content: 'world1',
+        isMe: false,
+      }"
+    />
+    <Msg
+      v-bind:msg="{
+        author: 'hello',
+        authorId: '',
+        color: '#f00',
+        createdAt: new Date(),
+        content: 'world2',
+        isMe: true,
+      }"
+    />
+  </div>
+</template>
+
+<style lang="scss"></style>
+
+<script lang="ts">
+import Vue from "vue";
+import Msg from "@/components/Msg.vue";
+
+export default Vue.extend({
+  name: "msg-list" as string,
+  components: {
+    Msg,
+  },
+});
+</script>
