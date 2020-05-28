26ff907c9baa7b074b9a6e8244a8aa9ac9ca1f73
Howard.Lee <yhunglee@gmail.com>
2020-05-29 07:26:35 +0800

    feat(SettingName.vue): setting nickname of an user.

---
+++ src/components/SettingName.vue
@@ -0,0 +1,25 @@
+<template>
+  <b-modal id="modal-name">
+    <b-row>
+      <label for="nickname">名稱</label>
+      <b-form-input v-model="nickname" id="nickname" placeholder="輸入暱稱">
+      </b-form-input>
+    </b-row>
+  </b-modal>
+</template>
+
+<style lang="scss" scope></style>
+
+<script lang="ts">
+import { Vue, Component } from "vue-property-decorator";
+
+@Component({
+  components: {},
+  props: [],
+})
+export default class SettingName extends Vue {
+  // TODO: find name and uuid for displaying and checking.
+
+  nickname = "";
+}
+</script>
