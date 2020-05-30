b845ceb02abf26e53f6fc72ff799a4b5a9af8cc5
Howard.Lee <yhunglee@gmail.com>
2020-05-30 14:15:22 +0800

    feat(db.ts): declare db with firestore utility

---
+++ src/db.ts
@@ -0,0 +1,11 @@
+import firebase from "firebase";
+import "firebase/firestore";
+import { firebaseConfig } from "@/utility/firebase-config";
+
+firebase.initializeApp({
+  projectId: firebaseConfig.projectId,
+  databaseURL: firebaseConfig.databaseURL,
+});
+// firebase.firestore().settings({ timestampsInSnapshots: true });
+
+export const db = firebase.firestore();
