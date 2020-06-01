82aafa3a9fd5fa88ba40e3bda569640c83ddd1a6
Howard.Lee <yhunglee@gmail.com>
2020-06-01 08:09:54 +0800

    feat(Navigator.vue): adjust styling for account tab.

--- src/components/Navigator.vue
+++ src/components/Navigator.vue
@@ -18,17 +18,64 @@
 <style lang="scss">
 .nav-item:nth-child(4) {
   // 帳號 position
-  margin-left: 75%;
+  margin-left: 72%;
   text-align: right;
   background-color: #21a;
-  .nav-link.disabled {
+  .nav-link.disabled a {
     color: #fff;
+  }
+}
+@media (max-width: 480px) {
+  .nav-item:nth-child(4) {
+    // 帳號 position
+    margin-left: 1%;
+    text-align: right;
+    background-color: #21a;
+    .nav-link.disabled a {
+      color: #fff;
+    }
+  }
+}
+
+@media (max-width: 767px) {
+  .nav-item:nth-child(4) {
+    // 帳號 position
+    margin-left: 42%;
+    text-align: right;
+    background-color: #21a;
+    .nav-link.disabled a {
+      color: #fff;
+    }
+  }
+}
+
+@media (min-width: 768px) and (max-width: 979px) {
+  .nav-item:nth-child(4) {
+    // 帳號 position
+    margin-left: 52%;
+    text-align: right;
+    background-color: #21a;
+    .nav-link.disabled a {
+      color: #fff;
+    }
+  }
+}
+
+@media (min-width: 980px) {
+  .nav-item:nth-child(4) {
+    // 帳號 position
+    margin-left: 30%;
+    text-align: right;
+    background-color: #21a;
+    .nav-link.disabled a {
+      color: #fff;
+    }
   }
 }
 </style>
 
 <script lang="ts">
-import { Vue, Component, Prop } from "vue-property-decorator";
+import { Vue, Component } from "vue-property-decorator";
 import ChatRoom from "@/components/ChatRoom.vue";
 import SettingName from "@/components/SettingName.vue";
 
